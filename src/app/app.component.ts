7692251158be96b2db30e6d6484d9aebd45d9fe4
Swetasmita <schinray@gmail.com>
2021-05-22 11:13:09 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo';
+  title = 'First REST API Project';
+  user: any
+  constructor(private api: ApiService){
+      }
+  ngOnInit(){
+    this.api.apiCall(). subscribe(data=> this.user = data);
+    console.log("Show API Data");
+  }
 }
